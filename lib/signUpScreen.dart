00742b794897e3c89b82f24049d56f9847480c97
Vutha Sothea <vuthasothea@gmail.com>
2024-04-10 22:29:57 +0700

    clean code

--- lib/signUpScreen.dart
+++ lib/signUpScreen.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:my_custom_project/loginScreen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -18,6 +16,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
   TextEditingController emailcontroller = new TextEditingController();
   TextEditingController pwdcontroller = new TextEditingController();
   bool isVisible = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,147 +27,162 @@ class _SignUpScreenState extends State<SignUpScreen> {
             height: 350,
             color: Color.fromRGBO(0, 105, 148, 1),
           ),
-          Container(
-            alignment: Alignment.topCenter,
-            margin: EdgeInsets.only(top: 80, left: 0, right: 0),
-            // top: 100,
-            // left: 0,
-            // right: 0,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  child: Column(
-                    children: [
-                      Image(image: AssetImage("images/signup.png"), width: 100,),
-                      SizedBox(
-                        height: 10,
+          ListView(
+            children: [
+              Container(
+                alignment: Alignment.topCenter,
+                margin: EdgeInsets.only(top: 40, left: 0, right: 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      child: Column(
+                        children: [
+                          Image(image: AssetImage("images/signup.png"), width: 100,),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text("Registration", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),),
+                        ],
                       ),
-                      Text("Registration", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width-50,
-                  height: 300,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 2,
-                          blurRadius: 2)]
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          controller: emailcontroller,
-                          style: TextStyle(fontSize: 16),
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(borderRadius: BorderRadius.circular(20),),
-                            hintText: "Email",
-                            prefixIcon: Icon(Icons.email_sharp, color: Colors.grey,),
-                          ),
-                          onChanged: (value){
-                            email = value;
-                          },
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        TextFormField(
-                          controller: pwdcontroller,
-                          obscureText: isVisible,
-                          style: TextStyle(fontSize: 16),
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(borderRadius: BorderRadius.circular(20),),
-                            hintText: "Password",
-                            prefixIcon: Icon(Icons.lock_sharp, color: Colors.grey,),
-                            suffixIcon: IconButton(onPressed: (){
-                                setState(() {
-                                  isVisible = !isVisible;
-                                });
-                            }, icon: isVisible?Icon(Icons.visibility):Icon(Icons.visibility_off))
-                          ),
-                          onChanged: (value){
-                            pwd = value;
-                          },
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Container(
-                            width: double.infinity,
-                            height: 50,
-                            child: ElevatedButton(
-                              onPressed: () async{
-                                final user = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: pwd);
-                                if(user == null){
-                                  FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: pwd);
-                                  alert_success(context);
-                                  // pwdcontroller.clear();
-                                  // emailcontroller.clear();
-                                } else if(user != null){
-                                  alert_unsuccess(context);
-                                }
-
-                              },
-                              child: Text("Sign Up", style: TextStyle(color: Colors.white),),
-                              style: ElevatedButton.styleFrom(backgroundColor: Color.fromRGBO(0, 105, 148, 1)),
-                            )
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width-50,
+                      height: 300,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: [BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 2,
+                              blurRadius: 2)]
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
                           children: [
-                            Text("Already have account?"),
-                            TextButton(onPressed: (){
-                              Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()));
-                            }, child: Text("Sign In"))
+                            TextFormField(
+                              controller: emailcontroller,
+                              style: TextStyle(fontSize: 16),
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(borderRadius: BorderRadius.circular(20),),
+                                hintText: "Email",
+                                prefixIcon: Icon(Icons.email_sharp, color: Colors.grey,),
+                              ),
+                              onChanged: (value){
+                                email = value;
+                              },
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            TextFormField(
+                              controller: pwdcontroller,
+                              obscureText: isVisible,
+                              style: TextStyle(fontSize: 16),
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(borderRadius: BorderRadius.circular(20),),
+                                hintText: "Password",
+                                prefixIcon: Icon(Icons.lock_sharp, color: Colors.grey,),
+                                suffixIcon: IconButton(onPressed: (){
+                                    setState(() {
+                                      isVisible = !isVisible;
+                                    });
+                                }, icon: isVisible?Icon(Icons.visibility):Icon(Icons.visibility_off))
+                              ),
+                              onChanged: (value){
+                                pwd = value;
+                              },
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Container(
+                                width: double.infinity,
+                                height: 50,
+                                child: ElevatedButton(
+                                  onPressed: () async{
+                                    await doSignup(context);
+                                  },
+                                  child: Text("Sign Up", style: TextStyle(color: Colors.white),),
+                                  style: ElevatedButton.styleFrom(backgroundColor: Color.fromRGBO(0, 105, 148, 1)),
+                                )
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text("Already have account?"),
+                                TextButton(onPressed: (){
+                                  Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()));
+                                }, child: Text("Sign In"))
+                              ],
+                            ),
                           ],
                         ),
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
         ],
       ),
     );
   }
+
+  Future<void> doSignup(BuildContext context) async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: pwd);
+      alert_success(context);
+    } on FirebaseAuthException catch(e) {
+      alert_unsuccess(context, e.message ?? "");
+    }
+  }
+
   void alert_success(BuildContext context){
     showDialog(context: context, builder: (context){
       return AlertDialog(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
         title: Column(
           children: [
-            Icon(Icons.check_circle_outline_rounded, color: Colors.green, size: 50,),
-            Text("Successfull")
+            Icon(Icons.check_circle, color: Colors.green, size: 50,)
           ],
         ),
+        content: Text("Signup successfully", textAlign: TextAlign.center),
+        actions: [
+          ElevatedButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text("Ok")
+          )
+        ],
       );
     });
   }
-  void alert_unsuccess(BuildContext context){
+  void alert_unsuccess(BuildContext context, String message){
     showDialog(context: context, builder: (context){
       return AlertDialog(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
         title: Column(
           children: [
-            Icon(Icons.incomplete_circle, color: Colors.red, size: 50,),
-            Text("This email is exist")
+            Icon(Icons.error, color: Colors.red, size: 50,),
           ],
         ),
+        content: Text(message, textAlign: TextAlign.center),
+        actions: [
+          ElevatedButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text("Ok")
+          )
+        ],
       );
     });
   }
+
 }
